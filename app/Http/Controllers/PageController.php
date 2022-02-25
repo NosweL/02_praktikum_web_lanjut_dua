6cda734df74709479428731654695dec8db057bf
Welson <welson160502@gmail.com>
2022-02-25 15:55:27 +0700

    update kedua

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,15 +6,5 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function index() {
-        echo 'Selamat Datang';
-    }
-
-    public function about() {
-        return "Welson Mario Naibaho <br> 2041720253";
-    }
-
-    public function articles($id){
-        return "Halaman Article dengan id ".$id;
-    }
+    //
 }
